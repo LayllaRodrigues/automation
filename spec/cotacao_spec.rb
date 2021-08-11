c625426e90162a7777041a581ef1e49a980d75cf
Laylla  de Oliveira Rodrigues <laylla.rodrigues@C0167.local>
2021-08-11 13:59:24 -0300

    vehicleOwnershipStatus

--- spec/cotacao_spec.rb
+++ spec/cotacao_spec.rb
@@ -13,23 +13,34 @@ describe 'Cotação auto por km -John Single' do
         find(:xpath, '//*[@id="root"]/div[2]/div/div[4]/div/div[2]/div/input').set"123.456.752-09"
         fill_in 'email' , with: 'laylla@youse.com.br'
         find(:xpath, '//*[@id="root"]/div[2]/div/div[6]/div/div[2]/div/input').set"11988776655"
-        
         find(:xpath, '//*[@id="insuredPersonMaritalStatus"]').click
         find(:xpath, '//*[@id="menu-"]/div[3]/ul/li[1]').click.set 'single'
 
+        #segurado é o motorista?
 
-   
         find(:xpath, '//*[@id="root"]/div[2]/div/div[8]/div/div[2]/div/label[2]/span[1]').click
 
+        #placa ou chassi?
+
         find(:xpath, '//*[@id="root"]/div[2]/div/div[8]/div/div[2]/div/label[1]/span[1]/span[1]').click
+
+        #placa 
         find(:xpath, '//*[@id="vehicleLicensePlate"]').set "YOU0000"
         
-        # usabilidade do veicuo
+        # tipo de uso do veiculo
 
         find(:id, 'vehicleUsage').click 
         find(:xpath, '//*[@id="menu-"]/div[3]/ul/li[1]').click.set 'particular'
 
-        # find(:xpath, '//*[@id="select-label"]').set 'particular'
+        #O veiculo esta em nome de
+
+        find(:xpath, '//*[@id="root"]/div[2]/div/div[9]/div[6]/div/div/div/div').click
+        find(:xpath, '//*[@id="menu-"]/div[3]/ul/li[1]').click.set 'pessoa fisica'
+
+        # find(:xpath, '//*[@id="vehicleOwnershipStatus"]').click.set 'Pessoa Física'
+        sleep 10
+
+
 
 
     end
