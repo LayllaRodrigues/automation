3c9d2d450f6eda988d501f6488a016da9a8ed1c2
Laylla  de Oliveira Rodrigues <laylla.rodrigues@ip-192-168-100-202.ec2.internal>
2021-08-11 02:16:05 -0300

    dados do motorista

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+D
+spec/cotacao_spec.rb,9/7/97e6d0372688ace024cd037bd4176995159b4cbd
--- spec/cotacao_spec.rb
+++ spec/cotacao_spec.rb
@@ -1,20 +1,53 @@
 describe 'Cotação auto por km -John Single' do
 
     before(:each) do
+        driver = Selenium::WebDriver.for :chrome
+        driver.manage.timeouts.implicit_wait = 10 # seconds
         visit 'https://stage-cotacao.youse.io/seguro-auto-por-km'
+        
     end
     
     it 'preenchimento de lead' do
 
         fill_in 'insuredPersonName' , with: 'John John'
-        find(:xpath, '//*[@id="root"]/div[2]/div/div[4]/div/div[2]/div/input').set("123.456.752-09")
+        find(:xpath, '//*[@id="root"]/div[2]/div/div[4]/div/div[2]/div/input').set"123.456.752-09"
         fill_in 'email' , with: 'laylla@youse.com.br'
-        find(:xpath, '//*[@id="root"]/div[2]/div/div[6]/div/div[2]/div/input').set("11988776655")
-        # find(:xpath, '//*[@id="insuredPersonMaritalStatus"]').click.set ('single')
+        find(:xpath, '//*[@id="root"]/div[2]/div/div[6]/div/div[2]/div/input').set"11988776655"
+        find(:xpath, '//*[@id="insuredPersonMaritalStatus"]').click
+        find(:xpath, '//*[@id="menu-"]/div[3]/ul/li[1]').click.set 'single'
 
-        find(:xpath, '//*[@id="root"]/div[2]/div/div[6]/div/div[2]/div/input').fill_in  with: 'single'
+
+   
+        find(:xpath, '//*[@id="root"]/div[2]/div/div[8]/div/div[2]/div/label[2]/span[1]').click
+        find(:xpath, '//*[@id="root"]/div[2]/div/div[8]/div/div[2]/div/label[1]/span[1]/span[1]').click
+        find(:xpath, '//*[@id="vehicleLicensePlate"]').set "YOU0000"
+
+        sleep 9
+        
+        # ajustar usabilidade do veicuo
+
+        find(:xpath, '//*[@id="root"]/div[2]/div/div[9]/div[5]/div/div/div/div').click
+        # find(:xpath, '//*[@id="root"]/div[2]/div/div[9]/div[5]/div/div/div/div').click..set 'only_private'
+        
+
+
+
+        #{}uso do veiculo
+
+        # find(:xpath, '///*[@id="vehicleUsage"]').click
+        # find(:xpath, '//*[@id="vehicleUsage"]').set 'only_private'
+        
+
+        # find(:xpath, '//*[@id="menu-"]/div[3]').click
+        # find(:xpath, '//*[@id="menu-"]/div[3]/ul/li[1]').click.set 
 
         
+
+
+        sleep 10
+
+
     end
 
 end
+
